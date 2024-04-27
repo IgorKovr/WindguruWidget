4c591c69ef0f716c570b1b26d76e83fd363d58a8
Igor Kovryzhkin <igorkovr@gmail.com>
2024-04-27 16:05:40 +0200

    Initial Commit

---
+++ Windguru/WindguruApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindguruApp.swift
+//  Windguru
+//
+//  Created by Igor Kovryzhkin on 27.04.24.
+//
+
+import SwiftUI
+
+@main
+struct WindguruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
